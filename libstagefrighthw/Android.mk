44df651fb60b8a1fea123819e5f6fd768563525e
C457 <android.c357@gmail.com>
2020-01-20 15:27:14 -0600

    libstagefrighthw: Include NV openmax headers
    * AOSP alternatives can possibly cause future issues
    even QC provides there own.

--- libstagefrighthw/Android.mk
+++ libstagefrighthw/Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := omxplugin.cpp
 LOCAL_C_INCLUDES += frameworks/native/include/media/hardware
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../openmax/include/openmax/il
 LOCAL_SHARED_LIBRARIES := libutils libdl liblog
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE := libstagefrighthw
